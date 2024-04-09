5bb990da61fdebc70a6011f6f613791e1e47dd2f
poweruser003 <162794694+poweruser003@users.noreply.github.com>
2024-04-09 13:56:58 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,34 +1,35 @@
 //2. For the above JSON, iterate over all for loops:
 
-//Iteration Using for loop:
-
-for(var i=0;i<myResume.length;i++){
-    console.log(myResume[i])
-   }
-
+//Using for loop:
+var keys= Object.keys(myResume)
+for(var i=0;i<keys.length;i++){
+    console.log(keys[i]+":"+myResume[keys[i]])
+  }
 
 //Using for-of loop:
-
-Obj_Keys=Object.keys(myResume)
-Obj_Entries=Object.entries(myResume)
+console.log("\n for of")
 for (const [key, value] of Object.entries(myResume)){
  console.log(`${key} : ${value}`);
 }
 
 
-//Using foreach loop:
+console.log("\n for each")
 
+//Using foreach loop:
 Object.entries(myResume).forEach(([key, value]) => {
  console.log(`${key} : ${value}`)
 })
 
-
+console.log("\n for in")
 
 //Using for in loop:
-
 for(var i in myResume){
-    console.log(myResume[i])
-   } 
+   console.log(`${i} : ${myResume[i]}`)
+    } 
+
+
+
+
 
 
 
